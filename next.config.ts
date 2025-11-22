6da24b24a4f3c69deae3bf163aecb0e449a9671b
PROFILE <72250349+Profility@users.noreply.github.com>
2025-11-22 11:07:00 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export"
+  // output: "export"
   // for static export ^^
 };
 
 export default nextConfig;
-x
