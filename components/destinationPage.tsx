70530718295c12452147288cff1f654ca152585b
PROFILE <72250349+Profility@users.noreply.github.com>
2025-12-09 08:16:07 +0800

    resizing description
    made mobile size smaller

--- components/destinationPage.tsx
+++ components/destinationPage.tsx
@@ -33,7 +33,7 @@ export function DestinationPage({ destination, image, title, description, embed}
         <div className="w-full mx-auto py-10 flex max-w-85 md:max-w-300 md:flex-row flex-col justify-center gap-26">
           <div className="md:w-160 md:pr-6">
             <h1 className="font-bold text-2xl">{title}</h1><br/>
-            <p className="text-xl text-justify">{description}</p>  
+            <p className="md:text-lg text-base text-justify">{description}</p>  
           </div>  
     
           <div>
