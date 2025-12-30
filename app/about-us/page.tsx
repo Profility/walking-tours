093dadabf22fd695d663566a242a0d8dc711b7d5
PROFILE <72250349+Profility@users.noreply.github.com>
2025-12-30 11:24:52 +0800

    fix about us img path

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div>
       <div className="relative w-full">
-          <ShowcaseCarousel images={"/kamaynihesus.jpg"} />
+          <ShowcaseCarousel images={"/kamay-ni-hesus.jpg"} />
       
           <h1 className="absolute top-1/2 md:left-10 transform -translate-y-1/2 text-5xl md:text-left text-center font-bold px-15 text-white text-shadow-md text-shadow-black pointer-events-none">
           <span className="text-4xl">About Us</span>
