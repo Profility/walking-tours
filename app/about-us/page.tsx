76b4b75480560510d57876ae117ec17bcdee7a7a
PROFILE <72250349+Profility@users.noreply.github.com>
2026-01-09 23:36:30 +0800

    Update page.tsx
    made it look better

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ShowcaseCarousel } from "@/components/showcaseCarousel";
-import { ExternalLink } from 'lucide-react';
+import { anchor } from "@/components/anchor";
 
 
 export default function Home() {
@@ -17,8 +17,11 @@ export default function Home() {
 
       <div className="w-full max-w-100 md:max-w-250 mx-auto py-10 px-4 md:text-lg text-base text-justify">
         <h1 className="font-bold text-2xl">About the Website</h1><br/>
-        <p>Lakbay Lucban is a QR-code web-based information system serving as centralized guides to enhance the trip planning and visitor experience in Lucban, Quezon by offering details about destinations selected by the Tourism Office of Lucban. This website was made as the output of a research conducted by Lucban Academy Senior Highschool Grade-12 Students to modernize the tourism experience in Lucban, Quezon.</p>
-        <br/><p>This website was created using the following technologies; NextJS as the front-end, TailwindCSS for the styling, and ShadCN as component library. Other miscellaneous technologies such as Google Maps integration through embeds, GitHub for version control, and Vercel for static-site hosting. All images showcasing each destinations are available publicly in their respective Facebook Pages. The code for this website is open-sourced and is available for viewing, it can be found <a className="text-blue-700" href="https://github.com/Profility/walking-tours">here{<ExternalLink className="inline-flex mx-1" size={16}/>}</a></p>      
+        <p>Lakbay Lucban is a QR-code web-based information system serving as a centralized guide to enhance the trip planning and visitor experience in Lucban, Quezon by offering details about destinations selected by the Tourism Office of Lucban. This website was made as the output of a research conducted by Lucban Academy Senior High School Grade-12 Students to modernize the tourism experience in Lucban, Quezon.</p>
+        <br/>
+        <p>
+          This website was developed using the following technologies: {anchor({ href: "https://nextjs.org/", children: "Next.js" })} as the front end, {anchor({ href: "https://tailwindcss.com/", children: "Tailwind CSS" })} for styling, and {anchor({ href: "https://ui.shadcn.com/", children: "shadcn/ui" })} as the component library. Other technologies include {anchor({ href: "https://www.google.com/maps/", children: "Google Maps" })} for map integration, {anchor({ href: "https://github.com/", children: "GitHub" })} for version control, {anchor({ href: "https://supabase.com", children: "Supabase" })} as the database, and {anchor({ href: "https://vercel.com/", children: "Vercel" })} for hosting. All images used on this website are publicly available from their respective Facebook pages, and proper credit is given. The source code for this website is open-source and can be found {anchor({ href: "https://github.com/Profility/walking-tours", children: " here" })}.
+        </p>    
       </div>
     </div>
   );
