f550803ed0b112e334ef5353a24aacbbd35912d7
PROFILE <72250349+Profility@users.noreply.github.com>
2026-01-12 08:09:14 +0800

    Update page.tsx

--- app/food/page.tsx
+++ app/food/page.tsx
@@ -12,13 +12,12 @@ export default async function Home() {
         <div className="flex justify-center mb-8">
           <h2 className="text-2xl font-bold">Local Food & Delicacies</h2>
         </div>
-        <hr/>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 py-8">
           {food.map((item, idx) => (
             <div
               key={idx}
               id={item.slug}
-              className="flex items-start space-x-4 border-b pb-4 last:border-b-0"
+              className="flex items-start space-x-4 pb-4 border-b last:border-b-0"
             >
               <div className="w-30 h-45 relative shrink-0">
                 <Image
